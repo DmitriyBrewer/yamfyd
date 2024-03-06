d04addd0ac8a06b1d30100a82c012635324ba486
DmitriyYanenko <paletobay@mail.ru>
2024-03-06 10:23:04 +0300

    select chat

--- src/components/chat-item/index.ts
+++ src/components/chat-item/index.ts
@@ -1,2 +1,42 @@
 import "./index.scss";
+
+document.addEventListener("DOMContentLoaded", function () {
+    const chatsList = document.querySelectorAll(".chat");
+
+    chatsList.forEach((chat) => {
+        chat.addEventListener("click", function () {
+            chatsList.forEach((chat) => {
+                chat.classList.remove("chat_active");
+            });
+
+            chat.classList.add("chat_active");
+            const chatMain = chat.querySelector(".chat__main");
+            const chatMinor = chat.querySelector(".chat__minor");
+
+            if (chatMain && chatMinor) {
+                const chatTitle =
+                    chatMain.querySelector(".chat__title")?.textContent ||
+                    "Нет данных";
+                const chatMessage =
+                    chatMain.querySelector(".chat__message")?.textContent ||
+                    "Нет данных";
+                const chatTime =
+                    chatMinor.querySelector(".chat__date")?.textContent ||
+                    "Нет данных";
+                const chatCounter =
+                    chatMinor.querySelector(".chat__counter")?.textContent ||
+                    "Нет данных";
+
+                console.log("Выбранный чат:");
+                console.log("Название: " + chatTitle);
+                console.log("Сообщение: " + chatMessage);
+                console.log("Время: " + chatTime);
+                console.log("Счетчик: " + chatCounter);
+            } else {
+                console.log("Ошибка: Не удалось найти данные чата");
+            }
+        });
+    });
+});
+
 export { default as ChatItem } from "./index.hbs?raw";
